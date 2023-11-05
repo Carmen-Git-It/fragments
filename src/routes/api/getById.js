b83202eaa8d795fa9ede34dc14a5efa74ad01f03
Carmen W <Carmenwhitton@gmail.com>
2023-11-05 16:36:18 -0500

    style: replaced promises with async/await for standardized style

--- src/routes/api/getById.js
+++ src/routes/api/getById.js
@@ -5,44 +5,41 @@ const logger = require('../../logger');
 /**
  * Get the data for a specific Fragment in a variety of formats
  */
-module.exports = (req, res) => {
+module.exports = async (req, res) => {
   logger.info('GET request to getById: ', req.params.id);
   // Split in case of fragmentID.txt or some other type
   let params = req.params.id.split('.');
   const id = params[0];
   // Get the fragment
-  Fragment.byId(req.user, id)
-    .then((fragment) => {
-      fragment.getData().then((data) => {
-        // if there is a filetype extension, change the content-type header accordingly
-        if (params.length > 1) {
-          if (params[1] === 'txt') {
-            res.set('content-type', 'text/plain');
-          } else {
-            logger.error('Invalid filetype in getById request: ', params[1]);
-            res
-              .status(415)
-              .json(
-                createErrorResponse(
-                  415,
-                  `Could not process fragment id:${id} as filetype .${params[1]}`
-                )
-              );
-            return;
-          }
-        } else {
-          res.set('content-type', data.type);
-        }
-        // Send the data with the corresponding content-type header
-        res.status(200).send(data);
-      });
-    })
-    .catch((e) => {
-      logger.error({ e }, `Could not find Fragment matching ownerId:${req.user} id:${id}`);
-      res
-        .status(404)
-        .json(
-          createErrorResponse(404, `Could not find Fragment matching ownerId:${req.user} id:${id}`)
-        );
-    });
+  try {
+    const fragment = await Fragment.byId(req.user, id);
+    const data = await fragment.getData();
+    if (params.length > 1) {
+      if (params[1] === 'txt') {
+        res.set('content-type', 'text/plain');
+      } else {
+        logger.error('Invalid filetype in getById request: ', params[1]);
+        res
+          .status(415)
+          .json(
+            createErrorResponse(
+              415,
+              `Could not process fragment id:${id} as filetype .${params[1]}`
+            )
+          );
+        return;
+      }
+    } else {
+      res.set('content-type', data.type);
+    }
+    // Send the data with the corresponding content-type header
+    res.status(200).send(data);
+  } catch (e) {
+    logger.error({ e }, `Could not find Fragment matching ownerId:${req.user} id:${id}`);
+    res
+      .status(404)
+      .json(
+        createErrorResponse(404, `Could not find Fragment matching ownerId:${req.user} id:${id}`)
+      );
+  }
 };
